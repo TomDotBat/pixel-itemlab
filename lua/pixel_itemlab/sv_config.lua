7c4d834510e2019ec743664335d7d0c4572fffcc
blu <75856885+bludotbat@users.noreply.github.com>
2021-02-10 20:34:44 -0600

    Testing Config change

--- lua/pixel_itemlab/sv_config.lua
+++ lua/pixel_itemlab/sv_config.lua
@@ -3,3 +3,4 @@ PIXEL.ItemLab.Config.FailedCraftTime = {10, 300}
 
 --item id, craftTimeMin, craftTimeMax, successRate (0-1), ingredient id 1, ingredient id 2, ingredient id 3
 PIXEL.ItemLab.Recipe("health50", 30, 50, 1, "melon", "melon", "melon")
+PIXEL.ItemLab.Recipe("pixel_armour_charger", 30, 50, 1, "cactus", "cactus", "cactus")
