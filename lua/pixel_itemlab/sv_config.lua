7ab7a36b0222afbb88a5d522de76ffd3f2e6523c
Tom O'Sullivan <tom@tomdotbat.dev>
2021-02-21 13:37:28 +0000

    Potential fix for broken recipes

--- lua/pixel_itemlab/sv_config.lua
+++ lua/pixel_itemlab/sv_config.lua
@@ -7,19 +7,19 @@ PIXEL.ItemLab.Recipe("health25", 30, 50, 1, nil, "melon", "energydrink", "baby")
 PIXEL.ItemLab.Recipe("health50", 45, 55, 1, nil, "melon", "melon", "energydrink")
 PIXEL.ItemLab.Recipe("healthmax", 60, 100, 1, nil, "melon", "baby", "baby")
 
-PIXEL.ItemLab.Recipe("armor25", 30, 50, 1, nil, "hook", "hook", "cactus")
-PIXEL.ItemLab.Recipe("armor50", 45, 100, 1, nil, "hook", "hook", "hook")
+PIXEL.ItemLab.Recipe("armor25", 30, 50, 1, nil, "pan", "pan", "cactus")
+PIXEL.ItemLab.Recipe("armor50", 45, 100, 1, nil, "pan", "pan", "pan")
 
-PIXEL.ItemLab.Recipe("pixel_armour_charger", 45, 100, 1, nil, "sign", "sign", "hook")
-PIXEL.ItemLab.Recipe("pixel_health_charger", 45, 100, 1, nil, "sign", "sign", "melon")
+PIXEL.ItemLab.Recipe("pixel_armour_charger", 45, 100, 1, nil, "hook", "hook", "pan")
+PIXEL.ItemLab.Recipe("pixel_health_charger", 45, 100, 1, nil, "hook", "hook", "melon")
 
 PIXEL.ItemLab.Recipe("fadedoorbomb", 180, 300, 1, nil, "briefcase", "briefcase", "vibrator")
 
-PIXEL.ItemLab.Recipe("pixel_armour_vip_charger", 45, 100, 1, PIXEL.CustomChecks.VIP, "sign", "hook", "hook")
-PIXEL.ItemLab.Recipe("pixel_health_vip_charger", 45, 100, 1, PIXEL.CustomChecks.VIP, "sign", "melon", "melon")
-PIXEL.ItemLab.Recipe("pixel_money_charger", 45, 100, 1, PIXEL.CustomChecks.VIP, "sign", "sign", "huladoll")
+PIXEL.ItemLab.Recipe("pixel_armour_vip_charger", 45, 100, 1, PIXEL.CustomChecks.VIP, "hook", "pan", "pan")
+PIXEL.ItemLab.Recipe("pixel_health_vip_charger", 45, 100, 1, PIXEL.CustomChecks.VIP, "hook", "melon", "melon")
+PIXEL.ItemLab.Recipe("pixel_money_charger", 45, 100, 1, PIXEL.CustomChecks.VIP, "hook", "hook", "huladoll")
 
-PIXEL.ItemLab.Recipe("pixel_reverse_money_charger", 45, 100, 1, PIXEL.CustomChecks.VIPPlus, "sign", "huladoll", "huladoll")
+PIXEL.ItemLab.Recipe("pixel_reverse_money_charger", 45, 100, 1, PIXEL.CustomChecks.VIPPlus, "hook", "huladoll", "huladoll")
 
 PIXEL.ItemLab.Recipe("fbiopenup", 30, 50, 1, nil, "baby")
 PIXEL.ItemLab.Recipe("fbiopenup", 30, 50, 1, nil, "briefcase", "vibrator", "baby")
