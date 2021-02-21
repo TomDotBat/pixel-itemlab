74ec06c6371fac5b1fe61e926d7bbb4f0db9d50e
blu <bluiscool1337@gmail.com>
2021-02-20 20:48:49 -0600

    Change Recipies

--- lua/pixel_itemlab/sv_config.lua
+++ lua/pixel_itemlab/sv_config.lua
@@ -7,15 +7,15 @@ PIXEL.ItemLab.Recipe("health25", 30, 50, 1, nil, "melon", "energydrink", "baby")
 PIXEL.ItemLab.Recipe("health50", 45, 55, 1, nil, "melon", "melon", "energydrink")
 PIXEL.ItemLab.Recipe("healthmax", 60, 100, 1, nil, "melon", "baby", "baby")
 
-PIXEL.ItemLab.Recipe("armor25", 30, 50, 1, nil, "pan", "pan", "cactus")
-PIXEL.ItemLab.Recipe("armor50", 45, 100, 1, nil, "pan", "pan", "pan")
+PIXEL.ItemLab.Recipe("armor25", 30, 50, 1, nil, "hook", "hook", "cactus")
+PIXEL.ItemLab.Recipe("armor50", 45, 100, 1, nil, "hook", "hook", "hook")
 
-PIXEL.ItemLab.Recipe("pixel_armour_charger", 45, 100, 1, nil, "sign", "sign", "pan")
+PIXEL.ItemLab.Recipe("pixel_armour_charger", 45, 100, 1, nil, "sign", "sign", "hook")
 PIXEL.ItemLab.Recipe("pixel_health_charger", 45, 100, 1, nil, "sign", "sign", "melon")
 
 PIXEL.ItemLab.Recipe("fadedoorbomb", 180, 300, 1, nil, "briefcase", "briefcase", "vibrator")
 
-PIXEL.ItemLab.Recipe("pixel_armour_vip_charger", 45, 100, 1, PIXEL.CustomChecks.VIP, "sign", "pan", "pan")
+PIXEL.ItemLab.Recipe("pixel_armour_vip_charger", 45, 100, 1, PIXEL.CustomChecks.VIP, "sign", "hook", "hook")
 PIXEL.ItemLab.Recipe("pixel_health_vip_charger", 45, 100, 1, PIXEL.CustomChecks.VIP, "sign", "melon", "melon")
 PIXEL.ItemLab.Recipe("pixel_money_charger", 45, 100, 1, PIXEL.CustomChecks.VIP, "sign", "sign", "huladoll")
 
